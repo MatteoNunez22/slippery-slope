20d8a44b8ab8a19696ccbbf2d08f1fb3f7f98016
Matteo Nunez <matteo.nunez22@gmail.com>
2019-01-22 21:00:58 -0500

    Added player and basic movement (E03)

---
+++ Slippery_Slope/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Reference to a rigidbody
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // FixedUpdate used because we are using physics
+    void FixedUpdate ()
+    {
+        // Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
